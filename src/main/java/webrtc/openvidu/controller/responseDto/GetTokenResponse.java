c6f8282698fb3e264b44e7da48199fc7ca4d1401
jojaeng2 <ds4ouj@naver.com>
2022-03-18 19:55:24 +0900

    Create Session controller

--- src/main/java/webrtc/openvidu/controller/responseDto/GetTokenResponse.java
+++ src/main/java/webrtc/openvidu/controller/responseDto/GetTokenResponse.java
@@ -1,4 +1,13 @@
 package webrtc.openvidu.controller.responseDto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class GetTokenResponse {
+
+    private String token;
+
+
 }
