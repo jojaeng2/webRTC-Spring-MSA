a134dbfefa7eb26543360325f5e5ed83405a750a
jojaeng2 <76645095+jojaeng2@users.noreply.github.com>
2022-04-01 23:59:11 +0900

    add swagger

--- src/main/java/webrtc/openvidu/config/SwaggerConfig.java
+++ src/main/java/webrtc/openvidu/config/SwaggerConfig.java
@@ -2,33 +2,20 @@ package webrtc.openvidu.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
-    public Docket api(){
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build().apiInfo(apiInfo());
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("WEBRTC REST API")
-                .version("1.0.0")
-                .description("WebRTC에 사용되는 REST API를 담당하는 Swagger 문서입니다.")
                 .build();
     }
 }
