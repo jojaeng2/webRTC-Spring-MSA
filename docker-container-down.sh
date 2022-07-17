c4db00124328ad1cb546f27b9399e0b99a9dbb75
jojaeng2 <ds4ouj@naver.com>
2022-07-17 10:46:50 +0900

    (2022-07-17) docker-compose-down.sh 스크립트 추가

---
+++ docker-container-down.sh
@@ -0,0 +1,37 @@
+echo ""
+echo "  _________                __         .__                          .___                               "
+echo "  \_   ___ \  ____   _____/  |______  |__| ____   ___________    __| _/______  _  ______              "
+echo "  /    \  \/ /  _ \ /    \   __\__  \ |  |/    \_/ __ \_  __ \  / __ |/  _ \ \/ \/ /    \             "
+echo "  \     \___(  <_> )   |  \  |  / __ \|  |   |  \  ___/|  | \/ / /_/ (  <_> )     /   |  \            "
+echo "   \______  /\____/|___|  /__| (____  /__|___|  /\___  >__|    \____ |\____/ \/\_/|___|  / /\  /\  /\ "
+echo "          \/            \/          \/        \/     \/             \/                 \/  \/  \/  \/ "
+
+sleep 3
+
+cd ./auth-service
+./docker-container-down.sh &
+cd ..
+sleep 3
+
+cd ./chat-service
+./docker-container-down.sh &
+cd ..
+
+sleep 3
+
+cd ./voice-service
+./docker-container-down.sh &
+cd ..
+sleep 3
+
+
+cd ./openvidu-service
+./docker-container-down.sh &
+cd .. 
+
+sleep 3
+
+
+cd ./nginx-service
+./docker-container-down.sh &
+cd ..
