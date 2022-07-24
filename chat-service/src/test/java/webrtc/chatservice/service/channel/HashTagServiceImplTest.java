8e0a3c5f50a122bf845f187d6e343fe9b8e387aa
jojaeng2 <ds4ouj@naver.com>
2022-07-24 21:33:08 +0900

    (2022-07-24) chat-service hashTagServiceTest 작성 완료

--- chat-service/src/test/java/webrtc/chatservice/service/channel/HashTagServiceImplTest.java
+++ chat-service/src/test/java/webrtc/chatservice/service/channel/HashTagServiceImplTest.java
@@ -1,62 +1,59 @@
-//package webrtc.chatservice.service.channel;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.transaction.annotation.Transactional;
-//import webrtc.chatservice.domain.HashTag;
-//import webrtc.chatservice.repository.hashtag.HashTagRepository;
-//import webrtc.chatservice.service.user.UserService;
-//
-//import java.util.List;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//@SpringBootTest
-//@Transactional
-//public class HashTagServiceImplTest {
-//
-//    @Autowired
-//    private HashTagService hashTagService;
-//    @Autowired
-//    private HashTagRepository hashTagRepository;
-//
-//    @Autowired
-//    private UserService userService;
-//
-//    @BeforeEach
-//    public void clearUserCache() {
-//        userService.redisDataEvict();
-//    }
-//
-//
-//    @Test
-//    @DisplayName("TagName으로 HashTag 찾기 성공")
-//    public void findHashTagByNameO() {
-//        // given
-//        HashTag hashTag = new HashTag("TestTagName");
-//
-//        // when
-//        hashTagRepository.save(hashTag);
-//        List<HashTag> hashTags = hashTagService.findHashTagByName("TestTagName");
-//
-//        // then
-//        assertThat(hashTag).isEqualTo(hashTags.get(0));
-//
-//    }
-//
-//    @Test
-//    @DisplayName("TagName으로 HashTag 찾기 실패")
-//    public void findHashTagByNameX() {
-//        // given
-//
-//        // when
-//        List<HashTag> hashTags = hashTagService.findHashTagByName("TestTagName");
-//
-//        // then
-//        assertThat(hashTags.size()).isEqualTo(0);
-//
-//    }
-//}
+package webrtc.chatservice.service.channel;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import webrtc.chatservice.domain.HashTag;
+import webrtc.chatservice.exception.HashTagException;
+import webrtc.chatservice.exception.HashTagException.NotExistHashTagException;
+import webrtc.chatservice.repository.hashtag.HashTagRepository;
+import webrtc.chatservice.service.user.UserService;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class HashTagServiceImplTest {
+
+    @Autowired
+    private HashTagService hashTagService;
+    @Autowired
+    private HashTagRepository hashTagRepository;
+
+    String tagName = "hashTag1";
+
+
+    @Test
+    @Transactional
+    public void HashTag_이름으로_조회성공() {
+        // given
+        HashTag hashTag = new HashTag(tagName);
+
+        // when
+        hashTagRepository.save(hashTag);
+        HashTag findHashTag = hashTagService.findHashTagByName(tagName);
+
+        // then
+        assertThat(findHashTag).isEqualTo(hashTag);
+
+    }
+
+    @Test
+    @Transactional
+    public void HashTag_이름으로_조회실패() {
+        // given
+
+        // when
+
+        // then
+        Assertions.assertThrows(NotExistHashTagException.class, ()->{
+            hashTagService.findHashTagByName(tagName);
+        });
+
+    }
+}
