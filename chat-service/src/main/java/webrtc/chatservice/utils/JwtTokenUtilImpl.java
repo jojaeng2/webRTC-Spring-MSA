523a8947af1eee680677a0fcd305727dcdc2bc7e
jojaeng2 <ds4ouj@naver.com>
2022-07-30 17:05:31 +0900

    (2022-07-30) nginx-service https 적용

--- chat-service/src/main/java/webrtc/chatservice/utils/JwtTokenUtilImpl.java
+++ chat-service/src/main/java/webrtc/chatservice/utils/JwtTokenUtilImpl.java
@@ -21,7 +21,7 @@ public class JwtTokenUtilImpl implements Serializable, JwtTokenUtil {
     private static final Long JWT_TOKEN_VALIDITY = 5 * 60 * 60L;
 
     @Value("${jwt.secret}")
-    private String secret = "govlepel@$&";
+    private static String secret = "govlepel@$&";
 
     //retrieve email from jwt-token
     public String getUserEmailFromToken(String token) {
