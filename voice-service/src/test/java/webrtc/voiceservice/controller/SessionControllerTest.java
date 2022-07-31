58bbc79070cb80fc699611a7f1303e26980c466a
jojaeng2 <ds4ouj@naver.com>
2022-07-31 20:39:48 +0900

    (2022-07-31) voice-service 토큰반남 문서작성 완료

--- voice-service/src/test/java/webrtc/voiceservice/controller/SessionControllerTest.java
+++ voice-service/src/test/java/webrtc/voiceservice/controller/SessionControllerTest.java
@@ -15,15 +15,16 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import webrtc.voiceservice.controller.session.SessionController;
 import webrtc.voiceservice.domain.User;
+import webrtc.voiceservice.dto.SessionDto;
 import webrtc.voiceservice.dto.SessionDto.GetTokenRequest;
+import webrtc.voiceservice.dto.SessionDto.RemoveUserInSessionRequest;
 import webrtc.voiceservice.exception.SessionException.OpenViduClientException;
 import webrtc.voiceservice.exception.UserException.NotExistUserException;
 import webrtc.voiceservice.service.session.OpenViduSessionService;
 
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
@@ -81,10 +82,7 @@ public class SessionControllerTest {
 
         // when
 
-
-
         // then
-
         mockMvc.perform(post("/api/v1/webrtc/voice/get-token")
                         .content(StrRequest)
                         .contentType(APPLICATION_JSON)
@@ -169,4 +167,74 @@ public class SessionControllerTest {
                         )
                 ));
     }
+
+
+
+    @Test
+    public void 유저퇴장성공() throws Exception {
+        // given
+
+        RemoveUserInSessionRequest ObjRequest = new RemoveUserInSessionRequest(sessionName1, email1, token);
+        String StrRequest = objectMapper.writeValueAsString(ObjRequest);
+
+        doReturn(new User(nickname1, password, email1))
+                .when(httpApiController).postFindUserByEmail(any(String.class));
+
+        doNothing()
+                .when(openViduSessionService).removeUserInOpenViduSession(any(RemoveUserInSessionRequest.class), any(User.class));
+
+        // when
+
+
+
+        // then
+
+        mockMvc.perform(post("/api/v1/webrtc/voice/remove-user")
+                        .content(StrRequest)
+                        .contentType(APPLICATION_JSON)
+                        .accept(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("removeuser-success",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                fieldWithPath("sessionName").type(STRING).description("세션 이름"),
+                                fieldWithPath("email").type(STRING).description("유저 Email"),
+                                fieldWithPath("token").type(STRING).description("발급받았던 토큰")
+                        )
+                ));
+    }
+
+    @Test
+    public void 유저퇴장실패_유저없음() throws Exception {
+        // given
+
+        RemoveUserInSessionRequest ObjRequest = new RemoveUserInSessionRequest(sessionName1, email1, token);
+        String StrRequest = objectMapper.writeValueAsString(ObjRequest);
+
+        doThrow(new NotExistUserException())
+                .when(httpApiController).postFindUserByEmail(any(String.class));
+
+        // when
+
+
+
+        // then
+
+        mockMvc.perform(post("/api/v1/webrtc/voice/remove-user")
+                        .content(StrRequest)
+                        .contentType(APPLICATION_JSON)
+                        .accept(APPLICATION_JSON))
+                .andExpect(status().is(404))
+                .andDo(document("removeuser-fail-NotExistUser",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                fieldWithPath("sessionName").type(STRING).description("세션 이름"),
+                                fieldWithPath("email").type(STRING).description("유저 Email"),
+                                fieldWithPath("token").type(STRING).description("발급받았던 토큰")
+                        )
+                ));
+    }
+
 }
