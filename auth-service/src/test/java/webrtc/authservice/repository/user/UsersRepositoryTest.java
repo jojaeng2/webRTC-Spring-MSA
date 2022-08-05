a0f2d391c8f8a52e30276f855abc38e20f7316a0
jojaeng2 <ds4ouj@naver.com>
2022-08-05 17:32:07 +0900

    (2022-08-05) auth-service database 정책 변경

--- auth-service/src/test/java/webrtc/authservice/repository/user/UsersRepositoryTest.java
+++ auth-service/src/test/java/webrtc/authservice/repository/user/UsersRepositoryTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import webrtc.authservice.domain.Point;
 import webrtc.authservice.domain.Users;
