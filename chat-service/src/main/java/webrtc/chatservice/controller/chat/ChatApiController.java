c854aa289703c658205828fc83a8e0b514b9a890
jojaeng2 <ds4ouj@naver.com>
2022-08-07 10:28:16 +0900

    (2022-08-07) chat-service swagger 문서 삭제

--- chat-service/src/main/java/webrtc/chatservice/controller/chat/ChatApiController.java
+++ chat-service/src/main/java/webrtc/chatservice/controller/chat/ChatApiController.java
@@ -16,15 +16,6 @@ public class ChatApiController {
 
     private final ChatService chatService;
 
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "채팅로그 목록을 정상적으로 반환합니다."),
-            @ApiResponse(code = 401, message = "jwt token에 아래의 4가지 문제중 하나가 존재합니다. 재로그인 후 토큰을 다시 발급받아 사용해야합니다. \n" +
-                    "1. 존재하지 않는 User token입니다. \n" +
-                    "2. Jwt Access Token이 만료되었습니다.\n" +
-                    "3. 올바르지 않은 Jwt Token 형식입니다. \n" +
-                    "4. 올바르지 않은 Jwt Hashing algorithm입니다."
-            )
-    })
     @GetMapping("/channel/{channelId}/{idx}")
     public ResponseEntity<FindChatLogsResponse> findChatLogs(@PathVariable("channelId") String channelId, @PathVariable("idx") String idx) {
         return new ResponseEntity<>(new FindChatLogsResponse(chatService.findChatLogsByIndex(channelId, Long.parseLong(idx))), HttpStatus.OK);
