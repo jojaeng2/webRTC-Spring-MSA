e4e396d5ee660eebd8422bb87aa2a2a1f6c85790
jojaeng2 <ds4ouj@naver.com>
2022-11-29 14:04:45 +0900

    refactor : HttpApiController 삭제후 DB에 접근하도록 코드 변경

--- chat-service/src/main/java/webrtc/chatservice/service/jwt/JwtUserDetailsService.java
+++ chat-service/src/main/java/webrtc/chatservice/service/jwt/JwtUserDetailsService.java
@@ -6,8 +6,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import webrtc.chatservice.controller.HttpApiController;
 import webrtc.chatservice.domain.Users;
+import webrtc.chatservice.exception.UserException;
+import webrtc.chatservice.exception.UserException.NotExistUserException;
+import webrtc.chatservice.repository.users.UsersRepository;
 
 import java.util.ArrayList;
 
@@ -15,11 +17,13 @@ import java.util.ArrayList;
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
 
-    private final HttpApiController httpApiController;
+    private final UsersRepository usersRepository;
+
     @Override
     @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
-        Users user = httpApiController.postFindUserByEmail(userEmail);
+        Users user = usersRepository.findByEmail(userEmail)
+                .orElseThrow(NotExistUserException::new);
         return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
     }
 }
