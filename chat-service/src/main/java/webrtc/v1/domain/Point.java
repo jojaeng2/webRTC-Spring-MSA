164bfbd2ca5b104af3f7ce3cbd88d6de208e8643
jojaeng2 <ds4ouj@naver.com>
2022-12-02 10:08:26 +0900

    feat : Point에 정적 메소드 팩토리 적용

--- chat-service/src/main/java/webrtc/v1/domain/Point.java
+++ chat-service/src/main/java/webrtc/v1/domain/Point.java
@@ -30,6 +30,25 @@ public class Point implements Serializable {
     @JoinColumn(name = "user_id")
     private Users user;
 
+    public static Point extensionChannelTTL(String email, Long ttl) {
+        final long pointUnit = 100L;
+
+        return Point.builder()
+                .message(email + " 님이 채널 연장에 포인트를 사용했습니다.")
+                .amount(-(int) (ttl * pointUnit))
+                .build();
+    }
+
+    public static Point createChannel(String email) {
+
+        final long pointUnit = 100L;
+        final long channelCreatePoint = 2L;
+        return Point.builder()
+                .message(email + " 님이 채널 생성에 포인트를 사용했습니다.")
+                .amount(-(int) (channelCreatePoint * pointUnit))
+                .build();
+    }
+
     public void setUser(Users user) {
         this.user = user;
     }
