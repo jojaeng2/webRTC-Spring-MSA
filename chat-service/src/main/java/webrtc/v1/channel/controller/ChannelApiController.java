590f0d90339b83540cc310b6312ce3852ebf317d
jojaeng2 <ds4ouj@naver.com>
2023-01-06 11:17:42 +0900

    feat : Google-Java-Style 컨벤션 추가

--- chat-service/src/main/java/webrtc/v1/channel/controller/ChannelApiController.java
+++ chat-service/src/main/java/webrtc/v1/channel/controller/ChannelApiController.java
@@ -27,76 +27,61 @@ import static org.springframework.http.HttpStatus.OK;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ChannelApiController {
 
-    private final ChannelLifeService channelLifeService;
-    private final ChannelFindService channelFindService;
-    private final JwtTokenUtil jwtTokenUtil;
+  private final ChannelLifeService channelLifeService;
+  private final ChannelFindService channelFindService;
+  private final JwtTokenUtil jwtTokenUtil;
 
-    /**
-     * 채널 생성 API
-     */
-    @PostMapping("/channel")
-    public ResponseEntity<CreateChannelResponse> createChannel(
-            @RequestBody CreateChannelRequest request,
-            @RequestHeader("Authorization") String jwtAccessToken
-    ) {
-        String userId = getUserId(jwtAccessToken.substring(4));
-        Channel channel = channelLifeService.create(request, userId);
-        return new ResponseEntity<>(new CreateChannelResponse(channel), HttpStatus.OK);
-    }
+  @PostMapping("/channel")
+  public ResponseEntity<CreateChannelResponse> createChannel(
+      @RequestBody CreateChannelRequest request,
+      @RequestHeader("Authorization") String jwtAccessToken
+  ) {
+    String userId = getUserId(jwtAccessToken.substring(4));
+    Channel channel = channelLifeService.create(request, userId);
+    return new ResponseEntity<>(new CreateChannelResponse(channel), HttpStatus.OK);
+  }
 
-    /**
-     * 조건없이 채널 목록을 불러오는 API
-     */
-    @GetMapping("/channels/{orderType}/{idx}")
-    public ResponseEntity<FindAllChannelResponse> findAnyChannel(
-            @NotNull @PathVariable("orderType") String orderType,
-            @NotNull @PathVariable("idx") String idx
-    ) {
-        List<Channel> channels = channelFindService.findAnyChannel(orderType, Integer.parseInt(idx));
-        return new ResponseEntity<>(new FindAllChannelResponse(channels), HttpStatus.OK);
-    }
+  @GetMapping("/channels/{orderType}/{idx}")
+  public ResponseEntity<FindAllChannelResponse> findAnyChannel(
+      @NotNull @PathVariable("orderType") String orderType,
+      @NotNull @PathVariable("idx") String idx
+  ) {
+    List<Channel> channels = channelFindService.findAnyChannel(orderType, Integer.parseInt(idx));
+    return new ResponseEntity<>(new FindAllChannelResponse(channels), HttpStatus.OK);
+  }
 
-    /**
-     * 특정 사용자가 입장한 채널 목록을 불러오는 API
-     * jwt access token에 들어있는 userEmail로 회원을 구분
-     */
-    @GetMapping("/mychannel/{orderType}/{idx}")
-    public ResponseEntity<FindAllChannelResponse> findMyAllChannel(
-            @NotNull @PathVariable("orderType") String orderType,
-            @NotNull @RequestHeader("Authorization") String jwtAccessToken,
-            @NotNull @PathVariable("idx") String idx
-    ) {
-        String userId = getUserId(jwtAccessToken.substring(4));
-        List<Channel> channels = channelFindService.findMyChannel(orderType, userId, Integer.parseInt(idx));
-        return new ResponseEntity<>(new FindAllChannelResponse(channels), OK);
-    }
+  @GetMapping("/mychannel/{orderType}/{idx}")
+  public ResponseEntity<FindAllChannelResponse> findMyAllChannel(
+      @NotNull @PathVariable("orderType") String orderType,
+      @NotNull @RequestHeader("Authorization") String jwtAccessToken,
+      @NotNull @PathVariable("idx") String idx
+  ) {
+    String userId = getUserId(jwtAccessToken.substring(4));
+    List<Channel> channels = channelFindService.findMyChannel(orderType, userId,
+        Integer.parseInt(idx));
+    return new ResponseEntity<>(new FindAllChannelResponse(channels), OK);
+  }
 
-    /**
-     * 최근 채팅이 보내진 시간을 기준으로 채널 목록을 불러오는 API
-     */
-    @GetMapping("/recent/{orderType}/{idx}")
-    public ResponseEntity<FindAllChannelResponse> findChannelsRecentlyTalk(
-            @NotNull @PathVariable("orderType") String orderType,
-            @NotNull @PathVariable("idx") String idx
-    ) {
-        List<Channel> channels = channelFindService.findChannelsRecentlyTalk(orderType, Integer.parseInt(idx));
-        return new ResponseEntity<>(new FindAllChannelResponse(channels), OK);
-    }
+  @GetMapping("/recent/{orderType}/{idx}")
+  public ResponseEntity<FindAllChannelResponse> findChannelsRecentlyTalk(
+      @NotNull @PathVariable("orderType") String orderType,
+      @NotNull @PathVariable("idx") String idx
+  ) {
+    List<Channel> channels = channelFindService.findChannelsRecentlyTalk(orderType,
+        Integer.parseInt(idx));
+    return new ResponseEntity<>(new FindAllChannelResponse(channels), OK);
+  }
 
 
-    /**
-     * 특정 채널 하나의 정보를 반환하는 API
-     */
-    @GetMapping("/channel/{id}")
-    public ResponseEntity<ChannelResponse> findOneChannel(
-            @PathVariable("id") String channelId
-    ) {
-        Channel channel = channelFindService.findById(channelId);
-        return new ResponseEntity<>(new ChannelResponse(channel), OK);
-    }
+  @GetMapping("/channel/{id}")
+  public ResponseEntity<ChannelResponse> findOneChannel(
+      @PathVariable("id") String channelId
+  ) {
+    Channel channel = channelFindService.findById(channelId);
+    return new ResponseEntity<>(new ChannelResponse(channel), OK);
+  }
 
-    private String getUserId(String token) {
-        return jwtTokenUtil.getUserIdFromToken(token);
-    }
-
+  private String getUserId(String token) {
+    return jwtTokenUtil.getUserIdFromToken(token);
+  }
 }
