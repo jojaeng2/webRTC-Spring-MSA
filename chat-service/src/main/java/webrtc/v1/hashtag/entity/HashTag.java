9369433073ca306e61a58a95f603e083210323f3
jojaeng2 <ds4ouj@naver.com>
2023-01-08 14:47:05 +0900

    chore : HashTag domain 코드 정렬 및 불필요한 import 삭제

--- chat-service/src/main/java/webrtc/v1/hashtag/entity/HashTag.java
+++ chat-service/src/main/java/webrtc/v1/hashtag/entity/HashTag.java
@@ -1,15 +1,21 @@
 package webrtc.v1.hashtag.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import webrtc.v1.channel.entity.ChannelHashTag;
-
-import javax.persistence.*;
-import java.io.Serializable;
-import java.util.*;
 
 @Entity
 @Getter
@@ -19,19 +25,19 @@ import java.util.*;
 @Table(indexes = @Index(name = "tagName_index", columnList = "name"))
 public class HashTag implements Serializable {
 
-    @Id
-    @Column(name = "hashtag_id")
-    @JsonIgnore
-    @Builder.Default
-    private String id = UUID.randomUUID().toString();
-    private String name;
+  @Id
+  @Column(name = "hashtag_id")
+  @JsonIgnore
+  @Builder.Default
+  private String id = UUID.randomUUID().toString();
+  private String name;
 
-    @OneToMany(mappedBy = "hashTag")
-    @JsonIgnore
-    @Builder.Default
-    private List<ChannelHashTag> channelHashTags = new ArrayList<>();
+  @OneToMany(mappedBy = "hashTag")
+  @JsonIgnore
+  @Builder.Default
+  private List<ChannelHashTag> channelHashTags = new ArrayList<>();
 
-    public void addChannelHashTag(ChannelHashTag channelHashTag) {
-        this.channelHashTags.add(channelHashTag);
-    }
+  public void addChannelHashTag(ChannelHashTag channelHashTag) {
+    this.channelHashTags.add(channelHashTag);
+  }
 }
--- chat-service/src/main/java/webrtc/v1/hashtag/repository/HashTagRepository.java
+++ chat-service/src/main/java/webrtc/v1/hashtag/repository/HashTagRepository.java
@@ -1,11 +1,10 @@
 package webrtc.v1.hashtag.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import webrtc.v1.hashtag.entity.HashTag;
 
-import java.util.Optional;
-
 public interface HashTagRepository extends JpaRepository<HashTag, String> {
 
-    Optional<HashTag> findByName(String name);
+  Optional<HashTag> findByName(String name);
 }
