2c1955d3451ee855dedad4c887a10f72fedfdc5d
jojaeng2 <ds4ouj@naver.com>
2023-01-08 14:22:16 +0900

    refactor : UserController 삭제 후 사용하지 않는 메서드 삭제

--- chat-service/src/main/java/webrtc/v1/user/service/UsersService.java
+++ chat-service/src/main/java/webrtc/v1/user/service/UsersService.java
@@ -2,14 +2,8 @@ package webrtc.v1.user.service;
 
 
 import webrtc.v1.user.entity.Users;
-import webrtc.v1.user.dto.UsersDto.CreateUserRequest;
-
-import java.util.UUID;
 
 public interface UsersService {
-    Users save(CreateUserRequest request);
 
-    Users findOneById(String userId);
-
-    Users findOneByEmail(String email);
+  Users findOneById(String userId);
 }
--- chat-service/src/test/java/webrtc/v1/service/user/UsersServiceImplTest.java
+++ chat-service/src/test/java/webrtc/v1/service/user/UsersServiceImplTest.java
@@ -10,6 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 import webrtc.v1.point.entity.Point;
 import webrtc.v1.point.service.PointService;
+import webrtc.v1.staticgenarator.UserGenerator;
 import webrtc.v1.user.entity.Users;
 import webrtc.v1.user.dto.UsersDto.CreateUserRequest;
 import webrtc.v1.user.exception.UserException.NotExistUserException;
@@ -28,117 +29,25 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 public class UsersServiceImplTest {
 
-    @InjectMocks
-    private UsersServiceImpl userService;
-    @Mock
-    private ChannelRedisRepository channelRedisRepository;
-    @Mock
-    private UsersRepository usersRepository;
-    @Mock
-    private BCryptPasswordEncoder encoder;
-    @Mock
-    private PointService pointService;
+  @InjectMocks
+  private UsersServiceImpl userService;
+  @Mock
+  private UsersRepository usersRepository;
 
-    String nickname1 = "nickname1";
-    String password = "password";
-    String email1 = "email1";
-    @Test
-    public void 유저저장_성공() {
-        // given
-
-        CreateUserRequest request = new CreateUserRequest(nickname1, password, email1);
-
-        doReturn(password)
-                .when(encoder).encode(any(String.class));
-        doReturn(Users.builder()
-                .nickname(nickname1)
-                .password(password)
-                .email(email1)
-                .build())
-                .when(usersRepository)
-                .save(any(Users.class));
-
-        // when
-        Users response = userService.save(request);
-
-        // then
-        assertThat(request.getEmail()).isEqualTo(response.getEmail());
-    }
+  String nickname1 = "nickname1";
+  String password = "password";
 
 
-    @Test
-    public void 이메일로_유저찾기_성공() {
-        //given
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String email = "email";
+  @Test
+  public void 이메일로_유저찾기_성공() {
+    //given
 
-        doReturn(Optional.of(Users.builder()
-                .nickname(nickname1)
-                .password(password)
-                .email(email)
-                .build()))
-                .when(usersRepository).findById(any(String.class));
+    Users user = UserGenerator.createUsers();
 
-        //when
-        Users users2 = userService.findOneById("1");
+    //when
+    Users findUsers = userService.findOneById(user.getId());
 
-        //then
-        assertThat(users2.getEmail()).isEqualTo(email);
-    }
-
-//    @Test
-//    @Transactional
-//    public void 이메일로_유저와포인트_반환성공() {
-//        // given
-//        int point = 10000000;
-//
-//        Long channelTTL = 100000L;
-//        usersRepository.save(createUsers());
-//        doReturn(Optional.of(createUsers()))
-//                .when(usersRepository).findByEmail(any(String.class));
-//
-//        // when
-//        int response = pointService.findPointSum(email1);
-//
-//        // then
-//        assertThat(response).isEqualTo(point);
-//    }
-//
-//    @Test
-//    @Transactional
-//    public void 이메일로_유저정보와포인트_반환실패() {
-//        // given
-//        String id = "id";
-//        String email = "email";
-//        String channelId = "channelId";
-//        String nickname = "nickname";
-//        int point = 10000000;
-//
-//        Long channelTTL = 100000L;
-//
-//        // when
-//
-//        // then
-//        Assertions.assertThrows(NotExistUserException.class, ()-> {
-//            pointService.findPointSum(email);
-//        });
-//    }
-
-    Users createUsers() {
-        Users user = Users.builder()
-                .nickname(nickname1)
-                .password(password)
-                .email(email1)
-                .build();
-        user.addPoint(createPoint());
-        return user;
-    }
-
-    Point createPoint() {
-        return Point.builder()
-                .message("회원가입")
-                .amount(10000000)
-                .build();
-
-    }
+    //then
+    assertThat(user.getEmail()).isEqualTo(findUsers.getEmail());
+  }
 }
