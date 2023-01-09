58042a059683566e37d01b1db9c24a1e58f012f0
jojaeng2 <ds4ouj@naver.com>
2023-01-09 12:06:47 +0900

    test : RedisKeyExpire 테스트 생성

--- chat-service/src/main/java/webrtc/v1/utils/listner/RedisKeyExpiredListener.java
+++ chat-service/src/main/java/webrtc/v1/utils/listner/RedisKeyExpiredListener.java
@@ -1,26 +1,25 @@
 package webrtc.v1.utils.listner;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.listener.KeyExpirationEventMessageListener;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.stereotype.Component;
 import webrtc.v1.channel.service.ChannelLifeService;
-import webrtc.v1.chat.enums.ClientMessageType;
-import webrtc.v1.chat.service.ChattingService;
 
 @Component
 public class RedisKeyExpiredListener extends KeyExpirationEventMessageListener {
 
-    @Autowired
-    private ChannelLifeService channelLifeService;
+  private final ChannelLifeService channelLifeService;
 
-    public RedisKeyExpiredListener(@Qualifier("redisMessageListener")RedisMessageListenerContainer listenerContainer) {
-        super(listenerContainer);
-    }
+  public RedisKeyExpiredListener(
+      @Qualifier("redisMessageListener") RedisMessageListenerContainer listenerContainer,
+      ChannelLifeService channelLifeService) {
+    super(listenerContainer);
+    this.channelLifeService = channelLifeService;
+  }
 
-    @Override
-    public void doHandleMessage(org.springframework.data.redis.connection.Message message) {
-        channelLifeService.delete(message.toString());
-    }
+  @Override
+  public void doHandleMessage(org.springframework.data.redis.connection.Message message) {
+    channelLifeService.delete(message.toString());
+  }
 }
