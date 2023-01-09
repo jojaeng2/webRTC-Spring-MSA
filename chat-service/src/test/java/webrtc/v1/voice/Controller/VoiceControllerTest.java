1f92c7cb0182493453f762936556fc29e7322e27
jojaeng2 <ds4ouj@naver.com>
2023-01-09 18:39:26 +0900

    test : VoiceController 테스트 생성

--- chat-service/src/test/java/webrtc/v1/voice/Controller/VoiceControllerTest.java
+++ chat-service/src/test/java/webrtc/v1/voice/Controller/VoiceControllerTest.java
@@ -1,6 +1,27 @@
 package webrtc.v1.voice.Controller;
 
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.JsonFieldType.STRING;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,36 +38,18 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.filter.CharacterEncodingFilter;
-import webrtc.v1.user.entity.Users;
+import webrtc.v1.channel.entity.Channel;
+import webrtc.v1.staticgenarator.ChannelGenerator;
+import webrtc.v1.user.exception.UserException.NotExistUserException;
+import webrtc.v1.user.service.UsersService;
 import webrtc.v1.utils.jwt.JwtTokenUtilImpl;
 import webrtc.v1.utils.jwt.JwtUserDetailsService;
-import webrtc.v1.utils.log.trace.ThreadLocalLogTrace;
+import webrtc.v1.voice.controller.VoiceController;
 import webrtc.v1.voice.dto.VoiceRoomDto.GetTokenRequest;
 import webrtc.v1.voice.dto.VoiceRoomDto.RemoveUserInSessionRequest;
-import webrtc.v1.user.exception.UserException.NotExistUserException;
+import webrtc.v1.voice.exception.VoiceException.AlreadyRemovedSessionInOpenViduServer;
 import webrtc.v1.voice.exception.VoiceException.OpenViduClientException;
-import webrtc.v1.user.service.UsersService;
-import webrtc.v1.voice.controller.VoiceController;
 import webrtc.v1.voice.service.VoiceRoomService;
-
-import java.util.ArrayList;
-import java.util.UUID;
-
-import static org.hamcrest.Matchers.is;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.doThrow;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.restdocs.payload.JsonFieldType.STRING;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureRestDocs
 @ExtendWith(RestDocumentationExtension.class)
@@ -56,206 +59,192 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class VoiceControllerTest {
 
 
-    @InjectMocks
-    private VoiceController voiceController;
-    @Spy
-    private JwtTokenUtilImpl jwtTokenUtil;
-    @Mock
-    private JwtUserDetailsService jwtUserDetailsService;
-    @Mock
-    private VoiceRoomService voiceRoomService;
-    @Mock
-    private UsersService usersService;
+  @InjectMocks
+  private VoiceController voiceController;
+  @Spy
+  private JwtTokenUtilImpl jwtTokenUtil;
+  @Mock
+  private JwtUserDetailsService jwtUserDetailsService;
+  @Mock
+  private VoiceRoomService voiceRoomService;
+  @Mock
+  private UsersService usersService;
 
-    private MockMvc mockMvc;
-    private final ObjectMapper objectMapper = new ObjectMapper();
+  private MockMvc mockMvc;
+  private final ObjectMapper objectMapper = new ObjectMapper();
 
-    private final String nickname1 = "user1";
-    private final String password = "password";
-    private final String email1 = "email1";
-    private final String sessionName1 = "sessionName1";
-    private final String token = "token";
-    private final String uuid = UUID.randomUUID().toString();
-    String jwtAccessToken;
+  private final String password = "password";
+  private final String sessionName1 = "sessionName1";
+  private final String token = "token";
+  private final String uuid = UUID.randomUUID().toString();
+  String jwtAccessToken;
 
-    @BeforeEach
-    public void setup(RestDocumentationContextProvider restDocumentationContextProvider) throws Exception {
-        this.mockMvc = MockMvcBuilders.standaloneSetup(voiceController)
-                .apply(documentationConfiguration(restDocumentationContextProvider))
-                .addFilters(new CharacterEncodingFilter("UTF-8", true))
-                .build();
-        doReturn(new org.springframework.security.core.userdetails.User(uuid, password, new ArrayList<>()))
-                .when(jwtUserDetailsService).loadUserByUsername(any(String.class));
+  @BeforeEach
+  public void setup(RestDocumentationContextProvider restDocumentationContextProvider)
+      throws Exception {
+    this.mockMvc = MockMvcBuilders.standaloneSetup(voiceController)
+        .apply(documentationConfiguration(restDocumentationContextProvider))
+        .addFilters(new CharacterEncodingFilter("UTF-8", true))
+        .build();
+    doReturn(
+        new org.springframework.security.core.userdetails.User(uuid, password, new ArrayList<>()))
+        .when(jwtUserDetailsService).loadUserByUsername(any(String.class));
 
-        jwtAccessToken = jwtTokenUtil.generateToken(jwtUserDetailsService.loadUserByUsername(uuid));
-    }
-
-
-    @Test
-    public void 토큰발급성공() throws Exception {
-        // given
-
-        GetTokenRequest ObjRequest = new GetTokenRequest(sessionName1);
-        String StrRequest = objectMapper.writeValueAsString(ObjRequest);
-
-        doReturn(token)
-                .when(voiceRoomService).getToken(any(GetTokenRequest.class), any(String.class));
-
-        // when
-
-        // then
-        mockMvc.perform(post("/api/v1/webrtc/voice/get-token")
-                        .header(HttpHeaders.AUTHORIZATION, "jwt " + jwtAccessToken)
-                        .content(StrRequest)
-                        .contentType(APPLICATION_JSON)
-                        .accept(APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(document("create-token-success",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
-                        requestFields(
-                                fieldWithPath("channelId").type(STRING).description("세션 이름")
-                        ),
-                        responseFields(
-                                fieldWithPath("token").type(STRING).description("OpenVidu 서버와 통신하기 위해 필요한 token")
-                        )
-                ))
-                .andExpect(jsonPath("$.token", is(token)));
-    }
-
-    @Test
-    public void 토큰발급실패_유저없음() throws Exception {
-        // given
-
-        GetTokenRequest ObjRequest = new GetTokenRequest(sessionName1);
-        String StrRequest = objectMapper.writeValueAsString(ObjRequest);
-
-        doThrow(NotExistUserException.class)
-            .when(voiceRoomService).getToken(any(GetTokenRequest.class), any(String.class));
-        // when
+    jwtAccessToken = jwtTokenUtil.generateToken(jwtUserDetailsService.loadUserByUsername(uuid));
+  }
 
 
+  @Test
+  public void 토큰발급성공() throws Exception {
+    // given
 
-        // then
+    GetTokenRequest ObjRequest = new GetTokenRequest(sessionName1);
+    String StrRequest = objectMapper.writeValueAsString(ObjRequest);
 
-        mockMvc.perform(post("/api/v1/webrtc/voice/get-token")
-                        .header(HttpHeaders.AUTHORIZATION, "jwt " + jwtAccessToken)
-                        .content(StrRequest)
-                        .contentType(APPLICATION_JSON)
-                        .accept(APPLICATION_JSON))
-                .andExpect(status().is(404))
-                .andDo(document("create-token-fail-NotExistUser",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
-                        requestFields(
-                                fieldWithPath("channelId").type(STRING).description("세션 이름 - 채널 id를 넣어주시면 됩니다.")
-                        )
-                ));
-    }
+    doReturn(token)
+        .when(voiceRoomService).getToken(any(GetTokenRequest.class), any(String.class));
 
+    // when
 
-    @Test
-    public void 토큰발급실패_OpenVidu서버없음() throws Exception {
-        // given
+    // then
+    mockMvc.perform(post("/api/v1/webrtc/voice/get-token")
+            .header(HttpHeaders.AUTHORIZATION, "jwt " + jwtAccessToken)
+            .content(StrRequest)
+            .contentType(APPLICATION_JSON)
+            .accept(APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andDo(document("create-token-success",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint()),
+            requestFields(
+                fieldWithPath("channelId").type(STRING).description("세션 이름")
+            ),
+            responseFields(
+                fieldWithPath("token").type(STRING).description("OpenVidu 서버와 통신하기 위해 필요한 token")
+            )
+        ))
+        .andExpect(jsonPath("$.token", is(token)));
+  }
 
-        GetTokenRequest ObjRequest = new GetTokenRequest(sessionName1);
-        String StrRequest = objectMapper.writeValueAsString(ObjRequest);
+  @Test
+  public void 토큰발급실패_유저없음() throws Exception {
+    // given
 
-        doThrow(new OpenViduClientException())
-                .when(voiceRoomService).getToken(any(GetTokenRequest.class), any(String.class));
+    GetTokenRequest ObjRequest = new GetTokenRequest(sessionName1);
+    String StrRequest = objectMapper.writeValueAsString(ObjRequest);
 
-        // when
+    doThrow(NotExistUserException.class)
+        .when(voiceRoomService).getToken(any(GetTokenRequest.class), any(String.class));
+    // when
 
+    // then
 
-
-        // then
-
-        mockMvc.perform(post("/api/v1/webrtc/voice/get-token")
-                        .header(HttpHeaders.AUTHORIZATION, "jwt " + jwtAccessToken)
-                        .content(StrRequest)
-                        .contentType(APPLICATION_JSON)
-                        .accept(APPLICATION_JSON))
-                .andExpect(status().is(500))
-                .andDo(document("create-token-fail-NotExistOpenviduServer",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
-                        requestFields(
-                                fieldWithPath("channelId").type(STRING).description("세션 이름")
-                        )
-                ));
-    }
-
-
-
-    @Test
-    public void 유저퇴장성공() throws Exception {
-        // given
-
-        RemoveUserInSessionRequest ObjRequest = new RemoveUserInSessionRequest(sessionName1, token);
-        String StrRequest = objectMapper.writeValueAsString(ObjRequest);
-
-        doNothing()
-                .when(voiceRoomService).removeUserInVoiceRoom(any(RemoveUserInSessionRequest.class), any(String.class));
-
-        // when
+    mockMvc.perform(post("/api/v1/webrtc/voice/get-token")
+            .header(HttpHeaders.AUTHORIZATION, "jwt " + jwtAccessToken)
+            .content(StrRequest)
+            .contentType(APPLICATION_JSON)
+            .accept(APPLICATION_JSON))
+        .andExpect(status().is(404))
+        .andDo(document("create-token-fail-NotExistUser",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint()),
+            requestFields(
+                fieldWithPath("channelId").type(STRING).description("세션 이름 - 채널 id를 넣어주시면 됩니다.")
+            )
+        ));
+  }
 
 
+  @Test
+  public void 토큰발급실패_OpenVidu서버없음() throws Exception {
+    // given
 
-        // then
+    GetTokenRequest ObjRequest = new GetTokenRequest(sessionName1);
+    String StrRequest = objectMapper.writeValueAsString(ObjRequest);
 
-        mockMvc.perform(post("/api/v1/webrtc/voice/remove-user")
-                        .header(HttpHeaders.AUTHORIZATION, "jwt " + jwtAccessToken)
-                        .content(StrRequest)
-                        .contentType(APPLICATION_JSON)
-                        .accept(APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(document("removeuser-success",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
-                        requestFields(
-                                fieldWithPath("channelId").type(STRING).description("세션 이름"),
-                                fieldWithPath("token").type(STRING).description("발급받았던 토큰")
-                        )
-                ));
-    }
+    doThrow(new OpenViduClientException())
+        .when(voiceRoomService).getToken(any(GetTokenRequest.class), any(String.class));
 
-//    @Test
-//    public void 유저퇴장실패_유저없음() throws Exception {
-//        // given
-//
-//        RemoveUserInSessionRequest ObjRequest = new RemoveUserInSessionRequest(sessionName1, token);
-//        String StrRequest = objectMapper.writeValueAsString(ObjRequest);
-//
-//        doThrow(new NotExistUserException())
-//                .when(usersService).findOneByEmail(any(String.class));
-//
-//        // when
-//
-//
-//
-//        // then
-//
-//        mockMvc.perform(post("/api/v1/webrtc/voice/remove-user")
-//                        .header(HttpHeaders.AUTHORIZATION, "jwt " + jwtAccessToken)
-//                        .content(StrRequest)
-//                        .contentType(APPLICATION_JSON)
-//                        .accept(APPLICATION_JSON))
-//                .andExpect(status().is(404))
-//                .andDo(document("removeuser-fail-NotExistUser",
-//                        preprocessRequest(prettyPrint()),
-//                        preprocessResponse(prettyPrint()),
-//                        requestFields(
-//                                fieldWithPath("channelId").type(STRING).description("세션 이름"),
-//                                fieldWithPath("token").type(STRING).description("발급받았던 토큰")
-//                        )
-//                ));
-//    }
+    // when
 
-    Users createUsers() {
-        return Users.builder()
-                .nickname(nickname1)
-                .password(password)
-                .email(email1)
-                .build();
-    }
+    // then
+
+    mockMvc.perform(post("/api/v1/webrtc/voice/get-token")
+            .header(HttpHeaders.AUTHORIZATION, "jwt " + jwtAccessToken)
+            .content(StrRequest)
+            .contentType(APPLICATION_JSON)
+            .accept(APPLICATION_JSON))
+        .andExpect(status().is(500))
+        .andDo(document("create-token-fail-NotExistOpenviduServer",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint()),
+            requestFields(
+                fieldWithPath("channelId").type(STRING).description("세션 이름")
+            )
+        ));
+  }
+
+
+  @Test
+  public void 토큰삭제실패_VoiceRoom없음() throws Exception {
+    // given
+    Channel channel = ChannelGenerator.createTextChannel();
+    RemoveUserInSessionRequest request = new RemoveUserInSessionRequest(
+        channel.getId(), token);
+    String StrRequest = objectMapper.writeValueAsString(request);
+
+    doThrow(new AlreadyRemovedSessionInOpenViduServer())
+        .when(voiceRoomService)
+        .removeUserInVoiceRoom(any(RemoveUserInSessionRequest.class), any(String.class));
+
+    // when
+
+    // then
+
+    mockMvc.perform(post("/api/v1/webrtc/voice/remove-user")
+            .header(HttpHeaders.AUTHORIZATION, "jwt " + jwtAccessToken)
+            .content(StrRequest)
+            .contentType(APPLICATION_JSON)
+            .accept(APPLICATION_JSON))
+        .andExpect(status().is(404))
+        .andDo(document("remove-token-fail-AlreadyRemovedRoom",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint()),
+            requestFields(
+                fieldWithPath("channelId").type(STRING).description("세션 이름"),
+                fieldWithPath("token").type(STRING).description("음성채팅 토큰")
+            )
+        ));
+  }
+
+  @Test
+  public void 유저퇴장성공() throws Exception {
+    // given
+
+    RemoveUserInSessionRequest ObjRequest = new RemoveUserInSessionRequest(sessionName1, token);
+    String StrRequest = objectMapper.writeValueAsString(ObjRequest);
+
+    doNothing()
+        .when(voiceRoomService)
+        .removeUserInVoiceRoom(any(RemoveUserInSessionRequest.class), any(String.class));
+
+    // when
+
+    // then
+
+    mockMvc.perform(post("/api/v1/webrtc/voice/remove-user")
+            .header(HttpHeaders.AUTHORIZATION, "jwt " + jwtAccessToken)
+            .content(StrRequest)
+            .contentType(APPLICATION_JSON)
+            .accept(APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andDo(document("removeuser-success",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint()),
+            requestFields(
+                fieldWithPath("channelId").type(STRING).description("세션 이름"),
+                fieldWithPath("token").type(STRING).description("발급받았던 토큰")
+            )
+        ));
+  }
 }
