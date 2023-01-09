29a2a4aa667e5e039d1c0d576c99947fcfda125b
jojaeng2 <ds4ouj@naver.com>
2023-01-09 12:07:43 +0900

    test : HashTagDto 테스트 생성

---
+++ chat-service/src/test/java/webrtc/v1/hashtag/dto/HashTagDtoTest.java
@@ -0,0 +1,15 @@
+package webrtc.v1.hashtag.dto;
+
+import org.junit.jupiter.api.Test;
+
+public class HashTagDtoTest {
+
+  @Test
+  void HashTagDto생성() {
+    // given
+    HashTagDto hashTagDto = new HashTagDto();
+    // when
+
+    // then
+  }
+}
