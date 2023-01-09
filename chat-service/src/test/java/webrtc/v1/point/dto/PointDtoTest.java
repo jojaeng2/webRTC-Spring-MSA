f689e16ea3ed379a4b3ea9464f34487e4143a73e
jojaeng2 <ds4ouj@naver.com>
2023-01-09 12:05:20 +0900

    test : PointDto Test 생성

---
+++ chat-service/src/test/java/webrtc/v1/point/dto/PointDtoTest.java
@@ -0,0 +1,19 @@
+package webrtc.v1.point.dto;
+
+import org.junit.jupiter.api.Test;
+
+public class PointDtoTest {
+
+
+  @Test
+  void pointDto생성() {
+    // given
+    PointDto pointDto = new PointDto();
+    // when
+
+    // then
+
+  }
+
+
+}
