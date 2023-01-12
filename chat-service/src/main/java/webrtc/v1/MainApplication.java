b026ab9935550ea442f1025c31b0dc2d112dba98
jojaeng2 <ds4ouj@naver.com>
2023-01-12 11:23:07 +0900

    refactor : Swagger 사용 철회

--- chat-service/src/main/java/webrtc/v1/MainApplication.java
+++ chat-service/src/main/java/webrtc/v1/MainApplication.java
@@ -3,10 +3,8 @@ package webrtc.v1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 @EnableCaching
 public class MainApplication {
 
